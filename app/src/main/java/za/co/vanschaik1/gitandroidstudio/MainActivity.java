82544a2b438ba0b40b8fdd8da47cc1de49990ea2
Alanvs <alanvs@icon.co.za>
2019-01-21 13:49:25 +0200

    MainActivity and VCS Xml File

--- app/src/main/java/za/co/vanschaik1/gitandroidstudio/MainActivity.java
+++ app/src/main/java/za/co/vanschaik1/gitandroidstudio/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("AvS", "Message");
+        Log.i("AvS", "Message");
 
     }
 }
